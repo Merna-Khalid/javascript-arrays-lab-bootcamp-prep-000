72d6b21c82132338427a8dee226823a9f1b4ab1c
Merna khaled <merna.khaled.5049@gmail.com>
2018-08-14 15:00:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name) {
   return kittens.push(name)
 }
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function appendKitten() {
+  
+}
